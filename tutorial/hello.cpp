0a61e0918718f1212d58b7b49bf200e5e7dcf9c3
qiao <guohuiqiao@gmail.com>
2016-03-11 15:43:04 +0100

    expose class whose constructor is with/without argument

--- tutorial/hello.cpp
+++ tutorial/hello.cpp
@@ -1,30 +1,85 @@
-//  Copyright Joel de Guzman 2002-2004. Distributed under the Boost
-//  Software License, Version 1.0. (See accompanying file LICENSE_1_0.txt
-//  or copy at http://www.boost.org/LICENSE_1_0.txt)
-//  Hello World Example from the tutorial
-//  [Joel de Guzman 10/9/2002]
 
+#include <boost/python.hpp>
+#include <iostream>
+#include <string>
 
-#include <boost/python/module.hpp>
-#include <boost/python/def.hpp>
+namespace { // Avoid cluttering the global namespace.
 
-namespace {
-
-	char const* greet()
+	//a simple struct
+	struct Element
 	{
-	   return "hello, world";
+		void setN(int N) {this->NTime = N;};
+		int getN() {return NTime;};
+		int NTime {0};
+	};
+
+	// a class with default constructor
+	class World
+	{
+	public:
+		World() {};
+		void set(std::string msg) { this->msg = msg; }
+		std::string greet() { return msg; }
+		std::string msg;
+	};
+	// A base class with constructor.
+	class hello
+	{
+	public:
+		hello(const std::string& country) { this->country = country; }
+		std::string greet() const { return "Hello from " + country; }
+	protected:
+		std::string country;
+	};
+
+	// derived class with default constructor
+	class myHello : public hello
+	{
+	public:
+		std::string myWarmGreeting() {return "this is my warm hello from " + country;};
+	};
+
+	// A function taking a hello object as an argument.
+	std::string invite(const hello& w) {
+		return w.greet() + "! Please come soon!";
 	}
 
 	int square (int n)
 	{
-	   return n*n;
+		return n*n;
 	}
 
 	BOOST_PYTHON_MODULE(hello_ext)
 	{
-	    using namespace boost::python;
-	    def("greet", greet);
-	    def("square", square);
+		using namespace boost::python;
+
+		// expose the struct
+		class_<Element>("Element")
+				.def("setN", &Element::setN)
+				.def("getN", &Element::getN)
+				;
+
+		// expose a class with default constructor
+		class_<World>("World")
+				.def("greet", &World::greet)
+				.def("set", &World::set)
+				;
+
+		// Create the Python type object for our extension class and define __init__ function.
+		class_<hello>("hello", init<std::string>())
+				.def("greet", &hello::greet)  // Add a regular member function.
+				.def("invite", invite);  // Add invite() as a regular function to the module.
+
+//         class_<myHello, bases<hello> >("myHello", init<std::string>())
+//                .def("myWarmGreeting", &myHello::myWarmGreeting);
+		//expose funcitons
+		def("invite", invite); // invite() can also be made a member of module!!!
+		def("square", square);
+
+//	    class_<myHello, bases<hello> >("myHello", init)
+		//        .def("myWarmGreeting",&myHello::myWarmGreeting);
+
+
 	}
 
 }
